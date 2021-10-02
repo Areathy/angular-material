dae8a5e69f746664fbf4902f3072d866fb4ef99d
Henrietha <areathy33@gmail.com>
2021-10-02 23:30:16 +0100

    Validations

--- src/app/components-booking/components-booking.component.ts
+++ src/app/components-booking/components-booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../services/countries.service';
 
 @Component({
@@ -15,9 +15,9 @@ export class ComponentsBookingComponent implements OnInit {
 
   constructor(private countriesService: CountriesService) {
     this.formGroup = new FormGroup({
-      email: new FormControl(null),
-      customerName: new FormControl(null),
-      country: new FormControl(null)
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      customerName: new FormControl(null, [Validators.required, Validators.maxLength(30), Validators.pattern('^[A-Za-z. ]*$')]),
+      country: new FormControl(null, [Validators.required])
     });
   }
 
@@ -29,6 +29,47 @@ export class ComponentsBookingComponent implements OnInit {
       (error) => {
         console.log(error);
       });
+  }
+
+  //returns the form control instance based on the control name
+  getFormControl(controlName: string): FormControl {
+    return this.formGroup.get(controlName) as FormControl;
+  }
+
+  //returns the error message based on the given control and error
+  getErrorMessage(controlName: string, errorType: string): string {
+    //controlName = "customerName"
+    //errorType = "required"
+    switch (controlName)  {
+      case "customerName":  {
+          if (errorType === "required")
+            return "You must specify <strong>Name</strong>";
+          else if (errorType === "maxlength")
+            return "<strong>Name</strong> can contain up to 30 characters only";
+          else if (errorType === "pattern")
+            return "<strong>Name</strong> can contain alphabets or dot (.) or space only";
+          else
+            return "";
+        }
+
+      case "email": {
+          if (errorType === "required")
+            return "<strong>Email</strong> can't be blank";
+          else if (errorType === "email")
+            return "<strong>Email</strong> should be in correct format. Eg: someone@example.com";
+          else
+            return "";
+        }
+
+      case "country":  {
+          if (errorType === "required")
+            return "You must choose a <strong>Country</strong>";
+          else
+            return "";
+        }
+
+      default: return "";
+    }
   }
 
 }
