c3172713f414974caaeb8054b571cf6cf70c0f45
Henrietha <areathy33@gmail.com>
2021-10-03 02:05:38 +0100

    Multiple Checkboxes with Indeterminate

--- src/app/components-booking/components-booking.component.ts
+++ src/app/components-booking/components-booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 import { CountriesService } from '../services/countries.service';
 import { CustomErrorStateMatcher } from "../helpers/customErrorStateMatcher";
 import { City } from '../models/City';
@@ -20,14 +20,60 @@ export class ComponentsBookingComponent implements OnInit {
   cities: City[] = [];
   isCitiesLoading: boolean = false;
 
+  //checkbox-group
+  hobbies: any[] = [
+    { id: 1, hobbyName: "Music" },
+    { id: 2, hobbyName: "Food" },
+    { id: 3, hobbyName: "Travel" },
+    { id: 4, hobbyName: "Pets" },
+    { id: 5, hobbyName: "Hiking" },
+  ];
+
   constructor(private countriesService: CountriesService, private citiesService: CitiesService) {
     this.formGroup = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       customerName: new FormControl(null, [Validators.required, Validators.maxLength(30), Validators.pattern('^[A-Za-z. ]*$')]),
       country: new FormControl(null, [Validators.required]),
       city: new FormControl(null),
-      receiveNewsLetters: new FormControl(null)
+      receiveNewsLetters: new FormControl(null),
+      hobbies: new FormArray([]),
+      allHobbies: new FormControl(false)
     });
+
+    //add form controls to form array
+    this.hobbies.forEach(() => {
+      this.hobbiesFormArray.push(new FormControl(false));
+    });
+  }
+
+  //returns the form array
+  get hobbiesFormArray(): FormArray {
+    return this.formGroup.get("hobbies") as FormArray;
+  }
+
+  //executes when the user clicks on "All" checkbox for hobbies
+  onAllHobbiesCheckBoxChange() {
+    this.hobbiesFormArray.controls.forEach((hobby, index) => {
+      this.hobbiesFormArray.at(index).patchValue(this.formGroup.value.allHobbies);
+    });
+  }
+
+  //returns true, if all hobby checkboxes are checked
+  allHobbiesSelected() {
+    return this.hobbiesFormArray.value.every(val => val === true); //[true, true, true, true, true]
+  }
+
+  //returns true, if all hobby checkboxes are unchecked
+  noHobbiesSelected() {
+    return this.hobbiesFormArray.value.every(val => val === false); //[false, false, false, false, false]
+  }
+
+  //executes when the user checks / unchecks any hobby checkbox
+  onHobbyChange(i) {
+    if (this.allHobbiesSelected())
+      this.formGroup.patchValue({ allHobbies: true });
+    else
+      this.formGroup.patchValue({ allHobbies: false });
   }
 
   ngOnInit(): void {
